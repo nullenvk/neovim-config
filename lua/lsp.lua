b19635e53212c55f7a2f5a36c851db3ee66d6545
nullenvk <nullenvk@protonmail.com>
2025-03-13 16:27:47 +0100

    Disable Python LSP's code style linting

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -9,13 +9,25 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'lua_ls', 'pylsp', 'ansiblels'}
+local servers = { 'clangd', 'rust_analyzer', 'lua_ls', 'ansiblels'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
     capabilities = capabilities,
   }
 end
+
+-- Python config
+lspconfig['pylsp'].setup {
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = { enabled = false },
+      }
+    }
+  }
+}
 
 -- luasnip setup
 local luasnip = require 'luasnip'
