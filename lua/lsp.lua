4d9f076f02bc81c4430aa917e929631e841a6e5e
nullenvk <nullenvk@protonmail.com>
2025-04-22 01:49:20 +0200

    add support for Haskell

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -9,7 +9,8 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'lua_ls', 'ansiblels', 'nil_ls'}
+local servers = { 'clangd', 'rust_analyzer', 'lua_ls', 'ansiblels', 'nil_ls',
+  'hls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
