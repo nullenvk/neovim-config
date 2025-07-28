60e573d5344899333f6bad20f71166d25e5efd85
nullenvk <nullenvk@protonmail.com>
2025-07-28 16:36:02 +0200

    Remove autocompletion altogether, refactoring

--- init.lua
+++ init.lua
@@ -1,10 +1,7 @@
 require "options"
-require "keymaps"
 require "plugins"
 require "colorscheme"
-require "lsp"
 require "treesitter"
 require "telescope"
-require "tree"
 
 require("bufferline").setup()
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,2 @@
--- define common options
-local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
-}
 
 
