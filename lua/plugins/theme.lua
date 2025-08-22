d0709161aaa0bd07584e3e6148b0f7128a7c9a15
nullenvk <nullenvk@protonmail.com>
2025-08-22 15:54:06 +0200

    Make background transparent

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,5 +3,10 @@ return {
   config = function ()
     vim.opt.background = "dark"
     vim.cmd.colorscheme "oxocarbon"
+
+    -- Transparent background
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
   end
 }
